1a643891dbd3e8433bf390579f6faf23bdfe25de
B3ns44d <abdsamad23bnsaad@gmail.com>
2021-10-25 09:25:08 +0100

    ref: photogrid, mainheader components
    chnage from useTypedText hook to TypingEffect in VMainHeader, ref to a simple photos array in photogrid component

--- src/shared/components/MainHeader/index.tsx
+++ src/shared/components/MainHeader/index.tsx
@@ -1,22 +1,21 @@
 import styles from './styles.module.scss';
-import useTypedText from '@hooks/useTypedText';
+import { TypingEffect } from '@shared/utils';
 
 const VMainHeader = () => {
-	const welcomeMessage = useTypedText('Hello everyone!', 50);
-	const subMessage = useTypedText("I'm Karolina.", 50, 200);
-	const message = useTypedText(
-		"Welcome on my portfolio page! I'm a Software Developer & Content Creator, currently working as TypeScript React Web Developer. Living in Cracow, Poland.",
-		30,
-		350
-	);
 	return (
 		<div className={styles.aboutInfo}>
 			<h1 className={styles.header}>
-				{welcomeMessage}
+				{TypingEffect('Hello everyone!', 50)}
 				<br />
-				{subMessage}
+				{TypingEffect("I'm Karolina.", 50, 200)}
 			</h1>
-			<h2>{message}</h2>
+			<h2>
+				{TypingEffect(
+					"Welcome on my portfolio page! I'm a Software Developer & Content Creator, currently working as TypeScript React Web Developer. Living in Cracow, Poland.",
+					30,
+					350
+				)}
+			</h2>
 		</div>
 	);
 };
